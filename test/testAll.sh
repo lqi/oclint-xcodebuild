3552d74bd48b485e6f211f19777c3e842c732eaf
Longyi Qi <lqi+git@longyiqi.com>
2014-03-16 17:04:40 -0500

    Suuport for arm64 to arm conversion

--- test/testAll.sh
+++ test/testAll.sh
@@ -9,3 +9,10 @@ test xcode-5.0-5A11314m-pchpch
 test full-project-1
 test full-project-2
 test product-name-with-space
+
+function test_with_arch_conversion {
+    ../oclint-xcodebuild -convert-arm64-to-arm $1
+    diff $1.json compile_commands.json
+}
+
+test_with_arch_conversion arm64
